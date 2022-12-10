9e202dc5de5db95321db507afebdc62c69d7ab8a
vjcitn <stvjc@channing.harvard.edu>
2022-12-10 13:40:28 -0500

    need fix to case of pkgname in basilisk setup

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,5 +1,5 @@
 
 # necessary for python module control
 bsklenv <- basilisk::BasiliskEnvironment(envname="bsklenv", packages="pandas==1.3.5",
-    pkgname="biochail", pip="hail==0.2.105")
+    pkgname="BiocHail", pip="hail==0.2.105")
 
