2e3f226dd5a375fc45b370d47ef99b6d7c74148c
vjcitn <stvjc@channing.harvard.edu>
2022-12-19 17:58:58 -0500

    new ref genome support

---
+++ R/rg_update.R
@@ -0,0 +1,10 @@
+#' update the reference genome for a hail instance
+#' @param hc hail context
+#' @param init character(1) valid name for a reference genome, defaults to "GRCh38"
+#' @param newjson character(1) path to a json spec of a reference genome [needs doc]
+#' @return a python list; the function is used for its side effect
+#' @export
+rg_update = function(hc, init="GRCh38", newjson=system.file("json/t2tAnVIL.json", package="BiocHail") {
+   rg = hc$get_reference(init)
+   rg$read(newjson)
+}
