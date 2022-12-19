c3d10f676711462877b74b9147431a52a665661f
vjcitn <stvjc@channing.harvard.edu>
2022-12-19 18:00:01 -0500

    fix typo

--- R/rg_update.R
+++ R/rg_update.R
@@ -4,7 +4,7 @@
 #' @param newjson character(1) path to a json spec of a reference genome [needs doc]
 #' @return a python list; the function is used for its side effect
 #' @export
-rg_update = function(hc, init="GRCh38", newjson=system.file("json/t2tAnVIL.json", package="BiocHail") {
+rg_update = function(hc, init="GRCh38", newjson=system.file("json/t2tAnVIL.json", package="BiocHail")) {
    rg = hc$get_reference(init)
    rg$read(newjson)
 }
