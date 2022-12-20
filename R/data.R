f981549208a9b1394008fe633498e1639b700c13
vjcitn <stvjc@channing.harvard.edu>
2022-12-20 06:43:51 -0500

    adding some PCA data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' HWE-normalized PCA scores for 3202 thousand-genomes samples genotyped with the 
+#' telomere-to-telomere reference 
+#' @note The genotypes are from a 1% sample of loci on chr17
+#' @docType data
+#' @format data.frame
+"pcs_38k"
+
+#' HWE-normalized PCA scores for 3202 thousand-genomes samples genotyped with the 
+#' telomere-to-telomere reference 
+#' @note The genotypes are from a 5% sample of loci on chr17
+#' @docType data
+#' @format data.frame
+"pcs_191k"
