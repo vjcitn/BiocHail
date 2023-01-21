115324753efa012d2b77bf1b95e0604894daf21b
vjcitn <stvjc@channing.harvard.edu>
2023-01-20 22:54:09 -0500

    add ukbb_pan_ancestry utilities

---
+++ R/ukbb_init.R
@@ -0,0 +1,15 @@
+#' initialize ukbb
+#' @import basilisk
+#' @note ukbb module may be passed around
+#' @examples
+#' ukbb <- ukbb_init()
+#' names(ukbb)
+#' @export
+ukbb_init = function() {
+ proc = basilisk::basiliskStart(bsklenv, testload="hail") # avoid package-specific import
+ #on.exit(basilisk::basiliskStop(proc))
+ basilisk::basiliskRun(proc, function() {
+     ukbb = reticulate::import("ukbb_pan_ancestry") 
+     ukbb
+   })
+}
