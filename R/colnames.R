49d1b74d432242287a8ef6b5564b80120efd9899
vjcitn <stvjc@channing.harvard.edu>
2023-03-17 16:28:00 -0400

    adding colnames and get_key operations for hail.table.Table

---
+++ R/colnames.R
@@ -0,0 +1,35 @@
+#' S3 support
+#' @export
+colnames = function(x, do.NULL = TRUE, prefix="col") UseMethod("colnames")
+
+#' S3 support
+#' @export
+colnames.default = function(...) base::colnames(...)
+
+#' acquire column names of a Hail Table
+#' @return character()
+#' @note writes one line of table to disk to retrieve field names
+#' @param x instance of hail.table.Table
+#' @export
+colnames.hail.table.Table = function(x, ...) {
+ tf = tempfile()
+ on.exit(unlink(tf))
+ zz = reticulate::py_capture_output(x$head(1L)$export(tf))
+ names(read.delim(tf))
+}
+
+#' S3 generic for get_key
+#' @param x anything
+#' @export
+get_key = function(x) UseMethod("get_key")
+
+#' S3 method for get_key
+#' @param x instance of hail.table.Table
+#' @return a list with elements names (names of keys) and key_df (data.frame of key values, with column names)
+#' @export
+get_key.hail.table.Table = function(x) {
+ tf = paste0(tempfile(), ".csv")
+ zz = reticulate::py_capture_output(x$key$export(tf))
+ ans = read.csv(tf)
+ list(names=names(ans), key_df=ans)
+}
--- R/data.R
+++ R/data.R
@@ -2,11 +2,7 @@
 #' telomere-to-telomere reference 
 #' @note The genotypes are from a 1% sample of loci on chr17
 #' @docType data
-<<<<<<< HEAD
-#' @usage data('pcs_38k')
-=======
 #' @usage data("pcs_38k")
->>>>>>> upstream/devel
 #' @format data.frame
 "pcs_38k"
 
@@ -14,21 +10,13 @@
 #' telomere-to-telomere reference 
 #' @note The genotypes are from a 5% sample of loci on chr17
 #' @docType data
-<<<<<<< HEAD
-#' @usage data('pcs_191k')
-=======
 #' @usage data("pcs_191k")
->>>>>>> upstream/devel
 #' @format data.frame
 "pcs_191k"
 
 #' data.frame with metadata about 3202 samples genotyped against T2T reference
 #' @note Source: index files described at `https://www.internationalgenome.org/data-portal/data-collection/30x-grch38`
 #' @docType data
-<<<<<<< HEAD
-#' @usage data('kg_3202')
-=======
 #' @usage data("kg_3202")
->>>>>>> upstream/devel
 #' @format data.frame
 "kg_3202"
