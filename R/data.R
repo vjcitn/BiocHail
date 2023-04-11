684ef52fa334aeb4970bc6de215d4a56513f8cae
vjcitn <stvjc@channing.harvard.edu>
2023-04-11 11:31:50 -0400

    dealing with BiocCheck warning

--- R/data.R
+++ R/data.R
@@ -3,6 +3,7 @@
 #' @note The genotypes are from a 1% sample of loci on chr17
 #' @docType data
 #' @usage data("pcs_38k")
+#' @return data.frame
 #' @examples
 #' data(pcs_38k)
 #' dim(pcs_38k)
@@ -14,6 +15,7 @@
 #' @note The genotypes are from a 5% sample of loci on chr17
 #' @docType data
 #' @usage data("pcs_191k")
+#' @return data.frame
 #' @format data.frame
 "pcs_191k"
 
@@ -21,5 +23,6 @@
 #' @note Source: index files described at `https://www.internationalgenome.org/data-portal/data-collection/30x-grch38`
 #' @docType data
 #' @usage data("kg_3202")
+#' @return data.frame
 #' @format data.frame
 "kg_3202"
