4f512a06121b3ea8586c371d9b2c4e31c1e60db1
vjcitn <stvjc@channing.harvard.edu>
2024-10-30 05:41:56 -0400

    update Hail version

--- tests/testthat/test_hl.R
+++ tests/testthat/test_hl.R
@@ -4,7 +4,7 @@ context("hail_init code works")
 
 test_that("hail_init succeeds and is idempotent", {
    hl = hail_init()
-   expect_true(substr(hl$version(),1,7)=="0.2.108")
+   expect_true(substr(hl$version(),1,7)=="0.2.133")
    hl = hail_init()
-   expect_true(substr(hl$version(),1,7)=="0.2.108")
+   expect_true(substr(hl$version(),1,7)=="0.2.133")
 })
